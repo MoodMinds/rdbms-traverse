aaed0801ed75b18ffa92523e44dcce1a1adb23b1
Ivan Tishchenko <moodminds@hotmail.com>
2022-04-20 09:39:09 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# rdbms-traverse
-RDBMS Traverse - Synchronous traversal implementation of the RDBMS Client SPI based on JDBC.
+# Synchronous Traversal implementation of the [RDBMS](https://github.com/MoodMinds/rdbms) SPI
+
+This implementation of the [RDBMS](https://github.com/MoodMinds/rdbms) SPI supports synchronous execution traversal.
+Based on the [Routes Traverse](https://github.com/MoodMinds/routes-traverse) and the JDBC API. Can be used as a client
+over JDBC that provides a "humane" API for database management.
+
+## Examples
+
+As well as for the [Routes Traverse](https://github.com/MoodMinds/routes-traverse) the materialized execution representation is
+`Traversable`:
+
+```java
+import org.moodminds.rdbms.RdbmsException;
+import org.moodminds.rdbms.route.Stream;
+import org.moodminds.rdbms.traverse.ConnectionSource;
+import org.moodminds.rdbms.traverse.route.Routes;
+
+import java.util.logging.Logger;
+
+class Sample {
+
+    static final Logger LOG = Logger.getLogger(Sample.class.getName());
+
+    static final Stream<String, RdbmsException> EMPLOYEES = $ -> $
+            .search($$ -> $$.query("SELECT * FROM Employee"))
+                .handle(employee -> $
+                    .effect(employee, e -> LOG.info(e.<String>get("id"))) // peek
+                    .supply(employee, e -> e.get("firstName")));
+
+    ConnectionSource connectionSource = null; // imagine it is instantiated somehow
+
+    public void traverse() {
+
+        new Routes(connectionSource).stream(EMPLOYEES) // materialize to Traversable<String, RdbmsException>
+                .traverse(() -> s -> LOG.info(s));
+    }
+}
+```
+
+## Maven configuration
+
+Artifacts can be found on [Maven Central](https://search.maven.org/) after publication.
+
+```xml
+<dependency>
+    <groupId>org.moodminds.route</groupId>
+    <artifactId>rdbms-traverse</artifactId>
+    <version>${version}</version>
+</dependency>
+```
+
+## Building from Source
+
+You may need to build from source to use **RDBMS Traverse** (until it is in Maven Central) with Maven and JDK 9 at least.
+
+## License
+This project is going to be released under version 2.0 of the [Apache License][l].
+
+[l]: https://www.apache.org/licenses/LICENSE-2.0
---
+++ src/main/java/org/moodminds/rdbms/traverse/route/Routes.java
@@ -0,0 +1,246 @@
+package org.moodminds.rdbms.traverse.route;
+
+import org.moodminds.route.Emitting;
+import org.moodminds.route.Flowing;
+import org.moodminds.route.traverse.TraversableEmittable;
+import org.moodminds.rdbms.traverse.ConnectionSource;
+import org.moodminds.util.Consumer1;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * The {@link org.moodminds.lang.Traversable} implementation of the {@link org.moodminds.route.Routes} interface
+ * backed by JDBC API.
+ */
+public class Routes implements org.moodminds.rdbms.route.Routes {
+
+    /**
+     * Connection source holder field;
+     */
+    private final ConnectionSource connectionSource;
+
+    private Integer fetchSize;
+    private Integer batchSize;
+
+    /**
+     * Construct the object with the specified {@link ConnectionSource}.
+     *
+     * @param connectionSource the {@link ConnectionSource} specified
+     */
+    public Routes(ConnectionSource connectionSource) {
+        this(connectionSource, config -> {});
+    }
+
+    /**
+     * Construct the object with the specified {@link ConnectionSource} and {@link Config} consuming configuration.
+     *
+     * @param connectionSource the specified {@link ConnectionSource}
+     * @param configuration the specified {@link Config} consuming configuration
+     */
+    public Routes(ConnectionSource connectionSource, Consumer1<? super Config> configuration) {
+        this.connectionSource = requireNonNull(connectionSource);
+        configuration.exec(new Config() {
+            @Override public Config fetch(int fetch) { Routes.this.fetchSize = fetch; return this; }
+            @Override public Config batch(int batch) { Routes.this.batchSize = batch; return this; }
+        });
+    }
+
+
+    /**
+     * The {@link Routes} configuration interface.
+     */
+    public interface Config {
+
+        /**
+         * Configure the default relation result fetch size.
+         *
+         * @param fetch the specified default relation result fetch size
+         * @return the configuration self-instance
+         */
+        Config fetch(int fetch);
+
+        /**
+         * Configure the default insert batch size.
+         *
+         * @param batch the specified the default insert batch size
+         * @return the configuration self-instance
+         */
+        Config batch(int batch);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route<? super Flow<E>, ? extends Emitting<? extends V>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <V, E extends RuntimeException> TraversableEmittable<V, E> stream(Route<? super Flow<E>, ? extends Emitting<? extends V>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route<? super Flow<E>, ? extends Emitting<? extends V>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route<? super Flow<E>, ? extends Flowing<?>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <E extends RuntimeException> TraversableEmittable<Void, E> action(Route<? super Flow<E>, ? extends Flowing<?>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route<? super Flow<E>, ? extends Flowing<?>> route) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route1<? super Flow<E>, I, ? extends Emitting<? extends V>> route, I value) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route1<? super Flow<E>, I, ? extends Flowing<?>> route, I value) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route2<? super Flow<E>, I1, I2, ? extends Emitting<? extends V>> route, I1 value1, I2 value2) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route2<? super Flow<E>, I1, I2, ? extends Flowing<?>> route, I1 value1, I2 value2) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route3<? super Flow<E>, I1, I2, I3, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route3<? super Flow<E>, I1, I2, I3, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route4<? super Flow<E>, I1, I2, I3, I4, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route4<? super Flow<E>, I1, I2, I3, I4, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route5<? super Flow<E>, I1, I2, I3, I4, I5, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route5<? super Flow<E>, I1, I2, I3, I4, I5, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route6<? super Flow<E>, I1, I2, I3, I4, I5, I6, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route6<? super Flow<E>, I1, I2, I3, I4, I5, I6, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route7<? super Flow<E>, I1, I2, I3, I4, I5, I6, I7, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route7<? super Flow<E>, I1, I2, I3, I4, I5, I6, I7, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, V, E extends Exception> TraversableEmittable<V, E> stream(org.moodminds.route.Route8<? super Flow<E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, E extends Exception> TraversableEmittable<Void, E> action(org.moodminds.route.Route8<? super Flow<E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
